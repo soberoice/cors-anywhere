be379c86d48a71b36db84c2b8279493b68c145b1
soberoice <137581615+soberoice@users.noreply.github.com>
2025-04-24 10:06:25 +0100

    Update server.js 55

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originWhitelist: [],
-  requireHeader: [],
+  requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
     'cookie',
